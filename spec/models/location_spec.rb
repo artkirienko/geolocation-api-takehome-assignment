5b9c467ff29d66aa45f063a61dec8aeeb798c7e2
Artem Kirienko <artkirienko@gmail.com>
2023-11-30 21:10:05 -0800

    Add model validation spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,5 +3,7 @@
 require "rails_helper"
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_uniqueness_of(:ip) }
+  end
 end
